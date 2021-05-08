ca74d75de2d90e69c645af03888cc0e274fcecd6
Eric Viera <srthompsoncode@gmail.com>
2021-05-08 19:53:35 -0300

    update const my secret

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
+const MY_SECRET = process.env.MY_SECRET
 
 app.get('/', (req, res) => {
-  res.send('Hello World! TEST_KEY = ' process.env.MY_SECRET)
+  res.send(`Hello World! TEST_KEY = ${MY_SECRET}`)
 })
 
 app.listen(PORT, () => {
